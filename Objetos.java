bf8a824782720c48f59cf0a4054d7733f22bec3a
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-01 16:06:32 -0300

    Create Objetos.java

---
+++ Objetos.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        boolean VouF = true;
+        byte a;
+        char letra = 'F';
+        int valor = 4500;
+        double x = 3.14;
+        a = 20;
+        System.out.printf("Valor de VouF (boolean): %b\n", VouF);
+        System.out.printf("Valor de a (byte): %d\n", a);
+        System.out.printf("Valor de letra (char): %c\n", letra);
+        System.out.printf("Valor de valor (int): %d\n", valor);
+        System.out.printf("Valor de x (double): %2.2f\n", x);
+    }
+}
