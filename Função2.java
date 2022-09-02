f27400bea6672f95bbfcafcc1599dc23cd33e187
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-02 18:19:21 -0300

    Create Função2.java

---
+++ Função2.java
@@ -0,0 +1,26 @@
+/*                                       Função Java 2 Programas Geral em Java:
+
+Existem dois tipo de grupo, o programador e o usuarios. Quem vai só usar o java vai usar o, o ambiente de execução "Java" "JRE"(JavaRuntimeEnvironmente) e 
+quem vai programar o kit de desenvolvimento java o "JDK"(JavaDevelopmentKit).
+
+JRE é comporto por : BiBliotecas, maquina virtual java, vai fazer com que o codigo bytecode envie pras maquina , são apis de bibliotecas para utilizar
+em algum programa.
+E jvm "Loader" parte interna da "JVM" vai carretar o ByteCode na memoria da mquina virtual e o "verificador" executa sem problema nenhum.
+alem disso o interpretador de memória que pega o Bytecode e transforma pro codigo nativo da maquina. Exemplo: Jvm pro windows, converter o código bytecode
+numa instrução pro windows entender. E o gerenciador de memoria vai tratar os codigo e a variaveis pra serem gerenciada na memoro do JVM(ComputadorVirtual,
+ele tem memória).O gerenciador de memoria nao e do seu computador e sim da JVM. 
+
+Compilador Jit "JIT" "Just In Time" traduz em tempo real. pega os codigo importante compila dentro do bytecode, inves de utilizadr o compilador java vai 
+usar o "JIT".
+
+--------------------
+
+JDK: Ja vem com JRE e usa o Javalang e JavaTools.
+
+dentro do jdk ja tem o jre, ja tem o "runtime Environmente" que executa o programa,  quando instala jsk ja vem o jre. 
+Javac: compilador java transoforma codifo fonte e codigo java
+JavaTOOLS e o Debugger : verifica se o programa está sendo executavel em tempo real, teste e verifica conteudos de var ou acesso ao BD.
+
+ultimo são as "Ap´ís": distrivui varias "APIS" de desenvolvimento, conjunto de bibliotecas "JAVAFX", grupo de bibliotecas mega novo que inclui o JDK. ter
+varias APis. 
+IDE: Instala por opcão quando instal o jdk , ambiente de desenvolvimento que vai facilitar na hora de programar. */ 
