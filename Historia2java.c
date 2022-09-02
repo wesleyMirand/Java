b422033bda83c1fe4bc63625b960edb8c1c4d300
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-02 16:18:11 -0300

    Create Historia2java.c

---
+++ Historia2java.c
@@ -0,0 +1,24 @@
+                                               /*Pascal
+                                                
+Até que a evolução da computação e seus servidores, começou a dar erro para usar a linguagem "pascal", pois ela só aceita na plataforma dela a "MsDOS".
+Para fazer que o sistema funcionasse em outras plataforma "Linux, Windows, Apple", teria que fazer o mecanismo diferente. Até que então surgiu a linguagem
+"C", que é tão importante a ponto de estar no mercado até hoje.
+Exemplo código em c:
+*/
+
+    
+    #include <stdio.h>
+    void main () {
+      print("Olá, mundo!");
+    }
+
+/* A linguagem "C" tem peculiaridade especifica, tem compiladores para diversas plataformas. Então ao pegar um código fonte em "C", digitado e passar pelo
+compilador, de outos computadores, de outras plataformas, tipo no "LInux, WIndoWs, Apple", eles vão gerar resultados diferentes, cada código é especifico
+pra cada plataforma. Então ao pegar pegar esse códigos executaveis e enviar para sua plataforma, cada uma delas vai entender o programa criado em "C".
+
+EXEMPLO:
+ PEGAR CÓDGIGO EM C _____> COMPILAR em uma PLATAFORMA especifica______>PLataforma vai gerar um códifo executavel para tal Plataforma____>
+ 
+ E isso foi a revolução da programação, a linguagem "C" até hoje continua se destacando bastante. Por ser uma "Linguagem Multiplataforma", usando o 
+compilador certo ele vai funcionar para "Multiplas Plataformas". E em alguns casos tenque pegar o códifo em "C" e fazer pequenas adaptações, porcausa
+das plataformas diferentes. Ele deu nascimento ao famoso "FOREFOX" que tem multiplas plataformas, celular, computador foi tudo feito em "C".
