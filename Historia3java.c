d07ce8edddc914fb4dbd184e7c5fa2196b090b34
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-02 16:59:52 -0300

    Update and rename Historia2 to Historia3java.c

---
+++ Historia3java.c
@@ -0,0 +1,23 @@
+                                           /*  Evolução em C
+                                             
+ Devido a sua evolução surgiu, o "c++" trazendo mais recurso, acréscimo de "Orientação a Objetos" na linguagem, conceito moderno. A própria linguagem "c"
+tem um incremento que é o operador "++", que incrementa uma unidade. O "++" significa, somar mais um, que torna "C++", linguagem tradicional com acrescimo.
+Até que então a evolução continuo, levando mais dois "+" ao "c" se tornando "C++++"o famoso "C#"CSharp.
+ Até quem em "1990" uma empresa chamada "SunMicroSystem", queriam criar um dispositivo diferente. E assim interligar a linguagem "C++" e fazer varias 
+interfaces trabalharem com dispositivos diferentes entre si. Até que um grupo na empresa se crio o "Green Team" para criar um linguagem que substituice
+o "C++". Com nome de "Green Talk", com objetivo de intercomunicação, fazer que dipositivos diferentes, falassem entre si. Até que em "1991" o "GreenTalk"
+se torno um projeto enorme na empresa, que batizaram com um novo nome o "OaK"(Significado:Carvalho, Arvore), que pela ideia geraria bons fruto. Até ser 
+criado o primeiro modem tela de pc conhecido como (STARSEVEN, SIMPLESMENTE*7, Estrelinha 7). O aparelho foi revolucionário na época, parecia um "GPS" com 6
+& 7 polegadas. Tinha dispositivos: Antena de Cominicação, MicroShip Integrado, Cartões inteligentes, Entrada fone de Ouvido, Dispositivos Transmissão Infra-
+vermelho, sistema de som integrado e um tela sensivel ao toque. A tela tinha um sistema especifico, tela de boot do Star Seven, a pessoa podia apertas, 
+arrastar a tela e tinha varios componentes. E pra fazer coisas interativas, foi criado o mascote "DUKE", a pessoa controlava a casa inteira, aparti do
+aparelho "StarSeven", a intuição era uma tela (Sensível ao toque, permitia controlar eletrodomésticos e lampadas de casa. Mais infelizmente a midia não
+curtiu, e em 1992 o projeto foi engavetado. Até que em "1994", um famoso conhecido "Tim Berners-Lee" estava criando a linguagem "HTML" linguagem interativa
+a web e todas tecnologias envolvidas. E o "Star Seven" era interativo a linguagem OaK, permitia a interatividade. Até que ele pensou em juntar os dois,
+que se formou o projeto "WEBRUNNER".
+ O projeto da "Sun MicroSystems" se chamava "WebRunner" e era uma piada na época. Ela seria um navegador parecido com (firefox, Explorer), e em 1994 
+isso ainda não existia e eles queriam criar um interatividade na web. A ideia era pegar tudo que foi feito pra "Star Seven" e mandar pra implementar na
+"WEB" tudo perfeito. Até que surgiu um problema, ja tinha a linguagem "OaK", e na hora de registar o nome da linguagem, deu problena na "Goosling" não
+podia usar o nome e homenagear o frondoso "Carvalo" na frene da "Janela" do "Gosling", eles teriam que mudar o nome. O "OaK" ja não estava mais dísponivel,
+as duas equipe fizeram reunião e então surgiu o nome "Café", vindo do "CAFE FORTE" cultivado nos estados unidos, até então surgiu a famosa linguagem "JAVA".
+*/
