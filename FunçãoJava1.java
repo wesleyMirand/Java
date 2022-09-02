88440bb01272368325849956c2bff9461077d05a
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-02 17:56:56 -0300

    Create FunçãoJava1.java

---
+++ FunçãoJava1.java
@@ -0,0 +1,26 @@
+            /* Função em Java */
+            
+public static void main (String [] args) {
+  System.out.println ("Olá, mundo!") ;
+}
+
+
+/*  acima está um considerado, código fonte em "java" pra compilar é a mesma maneira de "c". Vamos precisar do compilador em javA(javaC) ou (javaCompiler)
+Ele vai pegar o código fonte, gerar um código binário que a maquina entenda, invés de "código executável", agente chama de "Bytecode". Se pegar o "bytecode"
+e enviar pro computador, ele ainda não vai entender. E o "bytecode" vai precisar de mais um software, a maquina de café famoso "Java Virtual Machine" ou 
+"Máquina Virtual Java". A "Java Virtual Machine" é conhecida pela sua sigla, "JVM". E ao pegar o "bytecode" enviar pro "Jvm"  o jvm vai criar um código
+pro computador e ai sim ele vai entender(compilar) e ai vai ser executavel.
+
+exemplo
+
+Código _> Banco:Javac _> Bytecode:compilador na maquina _> JVM _> COmputador
+
+
+___________________________________________________________________________________________________________________________________________________________
+ Existem "JVMs" para todos servidores e são maquinas virtual. O programador não precisa mexer nelas, pois ja são preparadas para executar qualquer código.
+ Funcionam em qualquer servidores, enviar pro compilador em qualquer plataforma e gerar o byte code. o byte code gerado pelo computador pode ser compilador
+ em qualquer maquina com jvm (relogios, computadores, eletrodomestico , celular).
+ 
+ A "WORA" Write Once Run Anywhere(Escreva uma vez e execute em qualquer lugar). Essa é vantagem do jvm do java, compila em qualquer lugar.
+ 
+*/
