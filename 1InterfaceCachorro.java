c38fbe1b8b05643622d4b5d01af10fe1a192cfb1
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-11-28 17:18:00 -0300

    Create 1InterfaceCachorro.java

---
+++ 1InterfaceCachorro.java
@@ -0,0 +1,27 @@
+public class Cachorro extends Mamifero implementes Animal domestico {
+	
+	private int tamanho;
+	private String raça;
+
+	public int getTamanho() {
+	     return tamanho;
+	}
+	public void setTamanho(int tamanho) {
+	     this.tamanho = tamanho;
+	}
+	public string getRaça() {
+	   return raça;
+	}
+	public void setRaça() {
+	   this.raça = raça;
+	}
+
+	@Override
+	public void amamentar() {
+
+	}
+	@Override 
+	public void emitirSom () {
+	   //TODO 
+	}
+}
