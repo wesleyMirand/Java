9760b643826246d96522317a40b938691edc34db
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-11-28 17:30:41 -0300

    Create abstract.java

---
+++ abstract.java
@@ -0,0 +1,10 @@
+public abstract class funcionario {
+	
+	protect  String nome;
+	protect double salario;
+
+	public Funcionario (String nome, double salario) {
+
+	 this.nome = nome;
+	 this.salario = salario;
+	}
