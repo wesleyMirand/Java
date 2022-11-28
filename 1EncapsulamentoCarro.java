f81b0a15aac25d0e14b35cb94e083d2da9e83350
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-11-28 16:31:53 -0300

    Create 1EncapsulamentoCarro.java

---
+++ 1EncapsulamentoCarro.java
@@ -0,0 +1,48 @@
+public class carro {
+	
+	private String marca;
+	private String modelo;
+	private int numPassageiros;
+	private double capCombustivel;
+	private double consumoCombustivel;
+
+    public String getMarca(){
+         return this.marca;
+    }	
+
+    public void setMarca(String marca) {
+          this.marca = marca;
+    }
+
+    public String getModelo(){
+         return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+          this.modelo = modelo;
+    }
+
+    public int getNumPassageiros () {
+              return this.NUmPassageiros;
+    }
+
+    public void NumPassageiros(int numPassageiros) {
+            this.numPassageiros = numPassageiros;
+    }
+
+    public double getCapCombustivel(){
+           return CapCombustivel;
+    }
+
+    public void setCapCombustivel() {
+           this.capCobbustivel = capCombustivel;
+    }
+
+        public double getConsumoCombustivel(){
+           return consumoCombustivel;
+    }
+
+    public void setConsumoCombustivel() {
+           this.consumoCombustivel = consumoCombustivel;
+    }
+}
