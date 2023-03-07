ec056e5ac1d8ffbf8dc1f18b8e31acfee00b3ec8
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-07 17:41:14 -0300

    Create ProcessamentoDeDados2.java

---
+++ ProcessamentoDeDados2.java
@@ -0,0 +1,17 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		int x;
+		double y;
+		
+		x = 5;
+		
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
