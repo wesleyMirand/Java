3343945b91334438db90492db73ca55b820637ec
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-07 17:41:56 -0300

    Create ProcessamentoDeDados4.java

---
+++ ProcessamentoDeDados4.java
@@ -0,0 +1,16 @@
+
+public class Main3float {
+
+	public static void main(String[] args) {
+		float b, B, h, area;
+		
+		b= 6f;
+		B= 8f;
+		h= 5f;
+		
+		area = (b + B) / 2f * h;
+		System.out.println(area);
+
+	}
+
+}
