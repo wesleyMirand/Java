727cf300fb650c33583f5bcfdf41f9029bf36f71
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-07 17:42:16 -0300

    Create ProcessamentoDeDados5.java

---
+++ ProcessamentoDeDados5.java
@@ -0,0 +1,17 @@
+
+public class Main4int {
+
+	public static void main(String[] args) {
+		int a, b;
+		double resultado;
+		
+		a = 5;
+	    b = 2;
+	    
+	    resultado = a / b;
+	    
+	    System.out.println(resultado);  
+
+	}
+
+}
