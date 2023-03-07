2c0216670a7785736e72a03f7454fc5ecbd382cb
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-07 17:42:37 -0300

    Create ProcessamentoDeDados6.java

---
+++ ProcessamentoDeDados6.java
@@ -0,0 +1,15 @@
+public class Main4decimal {
+
+	public static void main(String[] args) {
+		int a, b;
+		double resultado;
+		
+		a = 5;
+	    b = 2;
+	    
+	    resultado = (double) a / b;
+	    
+	    System.out.println(resultado);  
+	}
+
+}
