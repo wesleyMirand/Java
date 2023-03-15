86f46e95847d6eee808e3cf169b4f70796459499
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-14 23:56:26 -0300

    Create Scanner2.java

---
+++ Scanner2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class scaner2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		x = sc.nextInt();
+		System.out.println("voce digitou:" + x);
+		sc.close();
+				
+
+	}
+
+}
