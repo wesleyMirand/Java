ef54ebc63c6ff8899ee8bc8375fd2848ae862215
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-14 23:57:32 -0300

    Create scanner5.java

---
+++ scanner5.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class scanner5 {
+
+    
+    public static void main(String[] args) {
+    	Locale.setDefault(Locale.US);
+    	Scanner sc = new Scanner(System.in);
+    
+    
+    String x;
+    int y;
+    double z;
+    
+    x = sc.next();
+    y = sc.nextInt();
+    z = sc.nextDouble();
+    
+    System.out.println("Dados digitados:");
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+	}
+
+}
