709ae396a39d220ac5709be84638df66eddbf52b
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-03-22 23:38:11 -0300

    Create condicional.java

---
+++ condicional.java
@@ -0,0 +1,13 @@
+
+public class condicional {
+
+	public static void main(String[] args) {
+    int x = 5;
+	       	System.out.println("Oi Bom Dia");
+    if( x > 0) {
+    	    System.out.println("Boa Tarde");
+    }
+            System.out.println("Boa Noite");
+	}
+
+}
